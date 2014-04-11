e127a17e3b9261ed837c90287165927adbfd7d42
Florian Dütsch <florian.duetsch@nix-wie-weg.de>
2014-04-11 17:16:40 +0200

    Redis config from file

--- app.rb
+++ app.rb
@@ -4,13 +4,9 @@ require 'sidekiq'
 require 'open-uri'
 require 'aws-sdk'
 
-ec2 = AWS::EC2.new.regions['eu-west-1']
-instance_id = open('http://169.254.169.254/latest/meta-data/instance-id').read
-instance = ec2.instances[instance_id]
-redis_url = instance.tags.to_h['ENV_REDIS_URL']
-
-Sidekiq.configure_server { |config| config.redis = { url: redis_url } }
-Sidekiq.configure_client { |config| config.redis = { url: redis_url, size: 1 } }
+require_relative 'config'
+Sidekiq.configure_server { |config| config.redis = { url: REDIS_URL} }
+Sidekiq.configure_client { |config| config.redis = { url: REDIS_URL, size: 1 } }
 
 class HardWorker
   include Sidekiq::Worker
